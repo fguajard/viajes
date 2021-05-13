2625cfd0de4643da33922c3783c1b91dc3a82427
fguajard <franciscogm31@gmail.com>
2021-05-13 01:13:09 -0400

    aagregad

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,22 @@
+// $(document).ready(function () {
+//   $("a").click(function (event) {
+//     event.preventDefault();
 
+//     var gato = this.hash;
+//     $("html").animate(
+//       {
+//         scrollTop: $(gato).offset().top,
+//       },
+//       800
+//     );
+//   });
+// });
 
-// comentario en javascript
+$("a").on("click", function (e) {
+    
+    e.preventDefault();
+    
+    const href = $(this).attr("href");
+    
+    $("html, body").animate({ scrollTop: $(href).offset().top }, 800);
+  });
